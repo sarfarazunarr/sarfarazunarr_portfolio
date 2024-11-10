a08133ceafb67a6f18d177fbf0e88ca328d05966
Sarfaraz Unar <sarfarazunarr@gmail.com>
2024-11-10 20:45:54 +0500

    Project Initialized

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+        <div id='logo'>Sarfaraz.</div>
+        <nav></nav>
+    </div>
+  )
+}
+
+export default Navbar
